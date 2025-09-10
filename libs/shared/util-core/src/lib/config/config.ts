1f5bfc4a05abcbc5e8f7a49bc19cefb4dbc4c716
jatleti <jatleti@gmail.com>
2025-09-10 12:53:39 +0200

    Update config.ts

--- libs/shared/util-core/src/lib/config/config.ts
+++ libs/shared/util-core/src/lib/config/config.ts
@@ -1,7 +1,7 @@
 export class Config {
-    public static apiKeyToken = '13g5nhdzfngTOfZx7GObVLaYGkiNk7uL';
-    public static localUserToken = 'noutlyCurrentUserToken';
-    public static localUserRefreshToken = 'noutlyCurrentUserRefreshToken';
-    public static localUserPermissions = 'prNoutly';
+    public static apiKeyToken = '98cf2641ed018b1c3a0021d1411d68a25f45fa37e27717052001ffdedd1d1f2c';
+    public static localUserToken = 'residenciaCurrentUserToken';
+    public static localUserRefreshToken = 'residenciaCurrentUserRefreshToken';
+    public static localUserPermissions = 'prResidencia';
     public static baseUrl = '/admin';
 }
